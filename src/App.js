fb8dbe702b2f75709e5348851e4e07208867132a
Jesus <jesus.ordonez@uabc.edu.mx>
2022-11-25 09:50:48 -0800

    Cambios en las rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import LogInBarber from './routes/barber/loginBarber';
+import LogInClient from './routes/client/logInClient';
 import AddBarber from './routes/gerente/addBarber';
-import Navbar from './routes/principal/navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <LogInClient/>
     </div>
   );
 }
