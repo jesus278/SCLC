8aceb6249aa90a0da2759b939afd3a54bcd3b39d
Jesus <jesus.ordonez@uabc.edu.mx>
2022-11-24 17:29:36 -0800

    Version 1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogInBarber from './routes/barber/loginBarber';
+import AddBarber from './routes/gerente/addBarber';
+import LogInGerente from './routes/gerente/logInGerente';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+
+      <Routes>
+        <Route  path="/Barbero" element={<LogInBarber/>} />
+      </Routes>
+      <Routes>
+        <Route  path="/Gerente"  element={<LogInGerente/>} />
+        
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
